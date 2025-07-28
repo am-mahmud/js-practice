43daec397b7cf3494cdbf87fe267b571b36a4d26
am-mahmud <am.mahmudasif@gmail.com>
2025-07-28 11:10:37 -0400

    first commit

---
+++ freeDrinks.js
@@ -0,0 +1,16 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let burgerPrice = 600;
+
+// Condition for getting coke
+
+if(burgerPrice > 500){
+    console.log('You will get free coke');
+} else{
+    console.log('You need to pay for coke');
+}
