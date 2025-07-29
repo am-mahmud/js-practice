e475d0482ea83a597c05819b19b2ac9a6ca451ba
am-mahmud <am.mahmudasif@gmail.com>
2025-07-29 17:26:21 -0400

    File updated

--- condition/busfareCalculator.js
+++ condition/busfareCalculator.js
@@ -6,3 +6,16 @@ Ticket fare Calculator
     - Senior citizens (age >= 60) gets a 15% Discount
     - Otherwise Regular ticket fare 800 tk
 */
+
+let busTicket = 800;
+let age = 0;
+
+if (age < 10){
+    console.log("Bus ticket is free");
+}else if(age >=60){
+    const discount = busTicket * 15 / 100;
+    const payAmount = busTicket - discount;
+    console.log("You need to pay", payAmount);
+}else{
+    console.log('Pay the regular fee 800Tk')
+}
--- while/1 commitment.js
+++ while/1 commitment.js
@@ -5,3 +5,10 @@
  */
 
 /*programming hero*/
+
+let num = 1;
+
+while (num <=60){
+    console.log("I will invest at least 6 hrs every single day for next 60 days!");
+    num++;
+}
